2b5aae62281750d1035c80349431c4f7a905b981
missytake <missytake@systemli.org>
2024-08-12 14:40:12 +0200

    add russian translation

---
+++ i18n/ru.js
@@ -0,0 +1,16 @@
+Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
+  ru: {
+    TitleText: 'Общайтесь в Delta Chat',
+    DescriptionText: 'Delta Chat - это приложение для обмена сообщениями, которое работает через электронную почту.',
+    FormBlurbText: 'Создайте инвайт-ссылку для Delta Chat',
+    StepCodeText: 'Откройте Delta Chat, зайдите в раздел <b>QR-код → показать QR-инвайт код → Сохранить в буфер обмена</b>',
+    StepPasteText: 'Вставьте ссылку в это поле:',
+    StepShareText: 'Поделитесь полученной ссылкой через любой канал',
+    ShareLinkText: 'Открыть ссылку',
+    JoinText: 'Поздороваться с',
+    JoinGroupText: 'Join',
+    ChatText: 'Открыть чат',
+    DownloadText: 'Скачать Delta Chat',
+  },
+});
+
