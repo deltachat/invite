8a2856819c853559123af7d1c8680487e4352ed9
72374 <250991390+72374@users.noreply.github.com>
2026-05-06 15:59:56 +0200

    Add german translation

---
+++ i18n/de.js
@@ -0,0 +1,15 @@
+Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
+  de: {
+    TitleText: 'Über Delta Chat verbinden',
+    DescriptionText: 'Delta Chat ist eine Messenger-App die über E-Mail funktioniert.',
+    FormBlurbText: 'Erstelle einen Einladungslink für Delta Chat',
+    StepCodeText: 'Öffne Delta Chat, dann navigiere zu <b>QR-Code → QR-Einladungscode → Link kopieren</b>',
+    StepPasteText: 'In das folgende Feld einfügen:',
+    StepShareText: 'Teile den generierten Link über einen beliebigen Kanal',
+    ShareLinkText: 'Öffne Link',
+    JoinText: 'Sage Hallo zu',
+    JoinGroupText: 'Beitreten zu',
+    ChatText: 'Chat öffnen',
+    DownloadText: 'Delta Chat herunterladen',
+  },
+});
