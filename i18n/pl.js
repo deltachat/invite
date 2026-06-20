71bb86f4fe092a8ead1fe229f47d4915014fb9d0
72374 <250991390+72374@users.noreply.github.com>
2026-06-20 12:51:54 +0200

    Add polish translation

---
+++ i18n/pl.js
@@ -0,0 +1,17 @@
+Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
+  pl: {
+    TitleText: 'Połącz się przez Delta Chat',
+    DescriptionText: 'Delta Chat jest aplikacją działająca na bazie e-mail',
+    FormBlurbText: 'Stwórz zaproszenie do Delta Chat',
+    StepCodeText: 'Otwórz Delta Chat, następnie przejdź do <b>QR kod → Pokaż QR kod zaproszenia → Skopiuj do schowka</b>',
+    StepPasteText: 'Wklej w to pole:',
+    StepShareText: 'Udostępnij wygenerowany link przez inne kanały',
+    ShareLinkText: 'Otwórz link',
+    JoinText: 'Powiedz cześć',
+    JoinGroupText: 'Dołącz',
+    ChatText: 'Otwórz czat',
+    DownloadText: 'Pobierz Delta Chat',
+    OtherDeviceText: 'Naciśnij jeśli masz Delta Chat na innym urządzeniu',
+    ScanQRText: 'Zeskanuj, aby otworzyć czat na innym urządzeniu',
+  },
+});
